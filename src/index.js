28b46ed9bb857aa912afc5f9a820407e7fa08ff2
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-06-23 16:43:48 +0500

    remove errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import "./style.css";
 import TVShowApp from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 window.addEventListener("DOMContentLoaded", () => {
     
     TVShowApp.initialize();
