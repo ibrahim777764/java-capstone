f0245794bcce9c8d3ba51100af2c2695e0dc9a87
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-06-23 12:43:18 +0500

    updated index,js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import TVShowApp from './App';
-window.addEventListener("load", () => {
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+window.addEventListener("DOMContentLoaded", () => {
     
     TVShowApp.initialize();
     
