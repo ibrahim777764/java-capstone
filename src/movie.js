349a20f7609b1e9093cd56f8c2f863c9b4e9ef65
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-06-23 12:43:59 +0500

    add file

--- src/movie.js
+++
@@ -1,9 +0,0 @@
-class Show {
-  constructor (showId, showTitle, showDescription, image, summary) {
-    this.showId = showId;
-    this.showTitle = showTitle;
-    this.showDescription = showDescription;
-    this.image = image;
-    this.summary = summary;
-  }
-}
